7a9541539dff9d997dd95badc3b3b93d0627ef54
Jeka-Boss <56686842+Jeka-Boss@users.noreply.github.com>
2021-09-21 21:07:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+lightTheme();
+r = true;
+
+function lightTheme(){
+    document.querySelector("body").classList.replace("dark","light");
+    document.querySelector("span").innerHTML = "light"
+
+     for(i = 0; i < document.querySelectorAll(".hello").length; i++){
+        document.querySelectorAll(".hello")[i].classList.replace("dark-mode","light-mode");
+     }
+    document.querySelector(".plain-text").classList.replace("dark-mode-plain","light-mode-plain");
+    document.querySelector("button").classList.replace("btn-dark","btn-light");
+    r = true;
+
+}
+
+
+
+function darkTheme(){
+    document.querySelector("body").classList.replace("light","dark");
+    document.querySelector("span").innerHTML = "dark"
+     for(i = 0; i < document.querySelectorAll(".hello").length; i++){
+        document.querySelectorAll(".hello")[i].classList.replace("light-mode","dark-mode");
+     }
+    document.querySelector(".plain-text").classList.replace("light-mode-plain","dark-mode-plain");
+    document.querySelector("button").classList.replace("btn-light","btn-dark");
+    r = false;
+
+}
+
+
+ function buttonChangeTheme(){
+if(r == true){
+    darkTheme();
+}else if(r == false){
+    lightTheme();
+}
+
+}
+
+
+document.querySelector("button").addEventListener("click", buttonChangeTheme);
+
